5fc3aaa0dd18b7fdbc5650eb36c7460e78a3aa4e
Dhvani Vora <dvora@splunk.com>
2023-06-01 11:31:00 +0530

    removed requests module dependency

--- jira_connector.py
+++ jira_connector.py
@@ -404,7 +404,7 @@ class JiraConnector(phantom.BaseConnector):
         try:
             self._jira.myself()
         except Exception as e:
-            self._set_jira_error(action_result, JIRA_ERROR_PROJECTS_INFO, e)
+            self._set_jira_error(action_result, JIRA_ERROR_SERVER_INFO, e)
             self.save_progress(JIRA_ERROR_CONNECTIVITY_TEST)
             return action_result.get_status()
 
